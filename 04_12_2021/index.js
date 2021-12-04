7015b6cf2455d7a24885f6802bad6895df366f75
diegomaranges <diegomaranges@gmail.com>
2021-12-04 16:38:47 -0300

    create graphic with crartjs

---
+++ 04_12_2021/index.js
@@ -0,0 +1,61 @@
+//class Participant {};
+/*
+var Participant = function (name) {
+    this.name = name;
+    this.chatroom = null;
+};
+
+Participant.prototype = {
+    send: function (message, to) {
+        this.chatroom.send(message, this, to);
+    },
+    receive: function (message, from) {
+        console.log(from.name + " to " + this.name + ": " + message);
+    }
+};
+
+console.log(Participant);
+console.log(Participant.prototype);*/
+var chart = require("chart.js").Chart;
+var bs = require("browser-sync").create();
+
+// .init starts the server
+bs.init({
+    server: "./"
+});
+
+/*const ctx = document.getElementById('myChart');*/
+/*const myChart = new chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});*/
