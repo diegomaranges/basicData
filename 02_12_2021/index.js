b340a1a08348e1f5923dd01f9f6d39362d75d9c0
diegomaranges <diegomaranges@gmail.com>
2021-12-07 23:27:19 -0300

    design patterns and this

---
+++ 02_12_2021/index.js
@@ -0,0 +1,25 @@
+toastr.success('The process has been saved.', 'Success');
+toastr["error"]('<div><input class="input-small" value="textbox"/>&nbsp;<a href="http://johnpapa.net" target="_blank">This is a hyperlink</a></div><div><button type="button" id="okBtn" class="btn btn-primary">Close me</button><button type="button" id="surpriseBtn" class="btn" style="margin: 0 8px 0 8px">Surprise me</button></div>');
+
+const myFunction = () => {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "10000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      }
+      toastr["error"]("Are you the six fingered man?")
+};
+
+myFunction();
---
+++ 07_12_2021/index.js
@@ -0,0 +1,80 @@
+function bike() {
+    console.log(this.name);
+}
+
+var name = "Ninja";
+var obj1 = {
+    name: "Pulsar",
+    bike: bike
+};
+var obj2 = {
+    name: "Gixxer",
+    bike: bike
+};
+
+bike(); // "Ninja"
+obj1.bike(); // "Pulsar"
+obj2.bike(); // "Gixxer"
+
+/**
+ * issues / errores / bugs
+ * requeriment
+ */
+
+function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+}
+
+// Usage:
+var bmw = new Car('M4', '2019', '1000');
+console.log(bmw.year);
+
+var testModule = (function () {
+    var counter = 0;
+    return {
+        incrementCounter: function () {
+            return++ counter;
+        },
+        resetCounter: function () {
+            counter = 0;
+        }
+    };
+})();
+
+// Usage:
+console.log(testModule.incrementCounter());
+console.log(testModule.incrementCounter());
+console.log(testModule.incrementCounter());
+console.log(testModule.incrementCounter());
+testModule.resetCounter();
+console.log(testModule.incrementCounter());
+console.log(testModule.incrementCounter());
+
+function Person(nombre, edad) {
+    this.name = nombre;
+    this.age = edad;
+    this.hello = () => {
+        console.log("hello!! " + this.name);
+    };
+    this.edad = () => {
+        console.log("tengo: " + this.age);
+    };
+    this.getName = () => {
+        return this.name
+    };
+    this.putName = (newName) => {
+        this.name = newName;
+    };
+}
+
+let diego = new Person("diego", 26);
+diego.hello();
+diego.edad();
+console.log(diego.name);
+console.log(diego.getName());
+diego.putName("Martin");
+console.log(diego.getName());
+
+let myArray = new Array(5); // [5]
